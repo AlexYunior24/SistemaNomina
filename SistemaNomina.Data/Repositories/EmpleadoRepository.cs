bac2168fa775d5ac0e672365da6da5704ea808d7
Abalbuena00 <balbuena.alexb@gmail.com>
2026-05-24 19:39:56 -0400

    feat: agregar interfaz IEmpleadoRepository y repositorio en memoria

---
+++ SistemaNomina.Data/Repositories/EmpleadoRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaNomina.Domain.Entities;
+using SistemaNomina.Domain.Interfaces;
+
+namespace SistemaNomina.Data.Repositories
+{
+    internal class EmpleadoRepository : IEmpleadoRepository
+    {
+        private readonly List<Empleado> _empleados = new();
+        private int _nextId = 1;
+        public void AgregarEmpleado(Empleado empleado)
+        {
+            empleado.Id = _nextId++;
+            _empleados.Add(empleado);
+        }
+        public Empleado? ObtenerPorId(int id)
+        {
+            return _empleados.FirstOrDefault(e => e.Id == id);
+        }
+        public IEnumerable<Empleado> ObtenerTodosLosEmpleados()
+        {
+            return _empleados;
+        }
+        public void ActualizarEmpleado(Empleado empleado)
+        {
+            var index = _empleados.FindIndex(e => e.Id == empleado.Id);
+            if (index == -1)
+            {
+                throw new KeyNotFoundException($"No se encontró el empleado con Id {empleado.Id}");
+            }
+
+            _empleados[index] = empleado;
+        }
+        public void EliminarEmpleado(int id)
+        {
+            var empleado = ObtenerPorId(id);
+            if (empleado == null)
+            {
+                throw new KeyNotFoundException($"No se encontró el empleado con Id {id}");
+            }
+            else
+            {
+                _empleados.Remove(empleado);
+            }
+        }
+
+    }
+}
---
+++ SistemaNomina.Domain/Interfaces/IEmpleadoRepository.cs
@@ -0,0 +1,18 @@
+﻿using SistemaNomina.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaNomina.Domain.Interfaces
+{
+    public interface IEmpleadoRepository
+    {
+        void AgregarEmpleado(Empleado empleado);
+        Empleado? ObtenerPorId(int id);
+        IEnumerable<Empleado> ObtenerTodosLosEmpleados();
+        void ActualizarEmpleado(Empleado empleado);
+        void EliminarEmpleado(int id);
+    }
+}
